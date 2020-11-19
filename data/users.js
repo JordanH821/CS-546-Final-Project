28f2502172b3208849b3e7f645b255b3da295223
Jordan Handwerger <jordanhandwerger@yahoo.com>
2020-11-19 13:18:18 -0500

    Add server side validate for signup - separate validation files into server side and client side - add validatation functions for input strings, phone numbers, and emails

--- data/users.js
+++ data/users.js
@@ -1,6 +1,11 @@
 const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
-const bcrypt = require('bcrypt');
+const { hashPassword } = require('../auth/auth');
+const {
+    validateStringInput,
+    validateEmail,
+    validatePhoneNumber,
+} = require('../validation/serverSide');
 
 async function createUser(
     firstName,
@@ -11,25 +16,35 @@ async function createUser(
     homeNumber,
     workNumber
 ) {
-    // firstName = validateStringInput(firstName);
-    // lastName = validateStringInput(firstName);
-    // email = validateEmail(email);
-    // validatePassword(password);
-    // validatePhoneNumber(mobileNumber);
-    // validatePhoneNumber(homeNumber);
-    // validatePhoneNumber(workNumber);
+    firstName = validateStringInput(firstName, 'First Name');
+    lastName = validateStringInput(lastName, 'Last Name');
+    email = validateEmail(email);
+    validateStringInput(password);
+    validatePhoneNumber(mobileNumber, 'Mobile');
+    validatePhoneNumber(homeNumber, 'Home');
+    console.log(workNumber);
+    validatePhoneNumber(workNumber, 'Work');
     let usersCollection = await users();
     const insertedInfo = await usersCollection.insertOne({
         firstName: firstName,
         lastName: lastName,
         email: email,
-        password: password, // TODO hash
+        hashedPassword: await hashPassword(password), // TODO hash
         phone: { mobile: mobileNumber, home: homeNumber, work: workNumber },
         tasks: [],
     });
     if (insertedInfo.insertedCount === 0) throw 'Could not create user';
-    // return await getUserById(insertedInfo.insertedId.toString());
-    return true;
+    return await getUserById(insertedInfo.insertedId);
+}
+
+async function getUserById(id) {
+    let usersCollection = await users();
+    const user = await usersCollection.findOne({ _id: id });
+    if (user) {
+        return user;
+    } else {
+        throw `no use exists with id (${id})`;
+    }
 }
 
 module.exports = { createUser };
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const { createUser } = require('../data/users');
+const {
+    validateStringInput,
+    validatePhoneNumber,
+    validateEmail,
+} = require('../validation/serverSide');
 
 router.get('/signup', async (req, res) => {
     res.render('users/signup');
 });
 
 router.post('/signup', async (req, res) => {
-    // validate input
-    const result = await createUser(
-        req.body.firstName,
-        req.body.lastName,
-        req.body.email,
-        req.body.password,
-        req.body.mobileNumber,
-        req.body.homeNumber,
-        req.body.workNumber
-    );
-    res.render('users/signup');
+    try {
+        // validate input
+        const rq = req.body;
+        const firstName = validateStringInput(rq.firstName, 'First Name');
+        const lastName = validateStringInput(rq.lastName, 'Last Name');
+        const email = validateEmail(rq.email);
+        validateStringInput(rq.password, 'Password'); // don't set password equal to the return so that the user can have whitespace in their password
+        validatePhoneNumber(rq.mobileNumber, 'Mobile');
+        validatePhoneNumber(rq.homeNumber, 'Home');
+        validatePhoneNumber(rq.workNumber, 'Work');
+        console.log('Passed route testing');
+        const newUser = await createUser(
+            firstName,
+            lastName,
+            email,
+            rq.password,
+            rq.mobileNumber,
+            rq.homeNumber,
+            rq.workNumber
+        );
+        req.session.user = newUser;
+        delete req.session.user.hashedPassword; // remove hashedPassword for security
+        delete req.session.user.tasks; // delete tasks so they are not passed around unnecessarily
+        res.redirect('/users/profile');
+    } catch (e) {
+        console.log(e);
+        console.log(req.body.workNumber);
+        res.render('users/signup', { user: req.body, error: e });
+    }
+});
+
+router.get('/profile', async (req, res) => {
+    console.log(req.session.user);
+    res.render('users/profile', { user: req.session.user });
 });
 
 module.exports = router;
