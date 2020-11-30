95bbfa72912b5d926d94bf2bb36e4179f1cb39c4
unknown <patrick.sommer16@gmail.com>
2020-11-30 09:59:13 -0500

    stopping task card from being re-assigned to the same column it was already in

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,6 @@
 
-var cardCurrentlyDragging;
+let columnCurrentlyDragging;
+let cardCurrentlyDragging;
 
 sizeColumns();
 
@@ -17,33 +18,43 @@ function sizeColumns() {
 }
 
 function dashboardAllowDrop(ev) {
-     ev.preventDefault();
+    ev.preventDefault();
 }
 
 function dashboardOnDrag(ev) {
     cardCurrentlyDragging = ev.target;
+    columnCurrentlyDragging = findColumnForTaskCard(cardCurrentlyDragging);
 }
 
 function dashboardOnDrop(ev) {
-    
-    // find column
-    let current = ev.target;
-    let column;
-    while (!column) {
-        if (current.classList.contains("column")) {
-            column = current;
-        } else {
-            if (current.parentNode) {
-                current = current.parentNode
-            } else {
-                break;
-            }
-        }
+    const column = findColumnForTaskCard(ev.target);
+
+    // do not re-drag task cards into the column it was already in
+    if (column == columnCurrentlyDragging) {
+        return;
     }
 
     // move task to column
     if (column) {
         column.appendChild(cardCurrentlyDragging);
         cardCurrentlyDragging.scrollIntoView();
+    }
+}
+
+function findColumnForTaskCard(taskCard) {
+    // find column
+    let current = taskCard;
+
+    // continue to traverse up until we find a column or there is no parent node
+    while (true) {
+        if (current.classList.contains("column")) {
+            return current;
+        } else {
+            if (current.parentNode) {
+                current = current.parentNode
+            } else {
+                return null;
+            }
+        }
     }
 }
