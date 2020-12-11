5b3989bc84cf1ce9c370ba98aa5f3050fc065119
Desae <aseda.oad@gmail.com>
2020-12-11 00:15:31 +0000

    added require for task route inside routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,25 @@
 const userRoutes = require('./users');
+const taskRoutes = require('./tasks');
 const dashboardRoutes = require('./dashboard');
 const { authenticationCheckRedirect } = require('./middleware');
 
-const constructorMethod = (app) => {
-    // configure routes
-    app.use('/users', userRoutes);
-    app.use('/dashboard', dashboardRoutes);
-    app.use(
-        '/',
-        authenticationCheckRedirect('/dashboard', false),
-        async (req, res) => {
-            res.render('landing');
-        }
-    );
-    // wildcard route 404
-    app.use('*', (req, res) => {
-        res.status(404).render('error/404', { title: 'Not Found' });
-    });
+const constructorMethod = app => {
+  // configure routes
+  app.use('/users', userRoutes);
+  app.use('/tasks', taskRoutes);
+  //app.use('/archive', taskRoutes);
+  app.use('/dashboard', dashboardRoutes);
+  app.use(
+    '/',
+    authenticationCheckRedirect('/dashboard', false),
+    async (req, res) => {
+      res.render('landing');
+    }
+  );
+  // wildcard route 404
+  app.use('*', (req, res) => {
+    res.status(404).render('error/404', { title: 'Not Found' });
+  });
 };
 
 module.exports = constructorMethod;
