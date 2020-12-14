463870d79dee643d864f246444767c0eb649f740
Jordan Handwerger <jordanhandwerger@yahoo.com>
2020-12-13 20:04:59 -0500

    WIP

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ hbs.handlebars.registerHelper('prettyPrintDate', function (date) {
 
 // https://stackoverflow.com/questions/34252817/handlebarsjs-check-if-a-string-is-equal-to-a-value
 hbs.handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
-    console.log(`${arg1} === ${arg2}`);
+    // console.log(`${arg1} === ${arg2}`);
     return arg1 == arg2 ? options.fn(this) : options.inverse(this);
 });
 
--- public/js/createTask.js
+++ public/js/createTask.js
@@ -1,47 +1,18 @@
-let createTaskForm = $('#createTaskForm'),
-  title = $('#title'),
-  description = $('#description'),
-  priority = $('#priority');
-(dueDate = $('#dueDate')),
-  (reminderDate = $('#reminderDate')),
-  (status = $('#status')),
-  (tags = $('#tags'));
-createTaskBtn = $('#createTask-btn');
-(error = $('.pop-error')), (assignee = $('#assignee'));
-
-// frontend validation
-//
-
-$(function() {
-  // Initialize form validation on the registration form.
-  // It has the name attribute "registration"
-  $("form[name='createTaskForm']").validate({
-    // Specify validation rules
-    rules: {
-      // The key name on the left side is the name attribute
-      // of an input field. Validation rules are defined
-      // on the right side
-      title: 'required',
-      description: 'required',
-      dueDate: 'required',
-      reminderDate: 'required',
-      assignee: 'required',
-      tags: 'required'
-    },
-
-    // Specify validation error messages
-    messages: {
-      title: 'Please enter task title',
-      description: 'Please task description',
-      dueDate: 'Please set task due date',
-      reminderDate: 'Please set task reminder date',
-      assignee: 'Please set assignee(s)',
-      tags: 'Please set tag(s)'
-    },
-    // Make sure the form is submitted to the destination defined
-    // in the "action" attribute of the form when valid
-    submitHandler: function(form) {
-      form.submit();
+$('#taskForm').on('submit', (event) => {
+    $('#errorDiv').hide();
+    $('#errorDiv').empty();
+    try {
+        validateStringInput($('#title').val().trim(), 'Title');
+        validateStringInput($('#description').val().trim(), 'Description');
+        // validateSelect($('#priority').val().trim(), 'Priority);
+        validateDate($('#dueDate').val().trim(), 'Due Date');
+        validateDate($('#reminderDate').val().trim(), 'Reminder Date');
+        // validateSelect($('#status').val().trim(), 'Status');
+        validateStringInput($('#assignee').val().trim(), 'Assignee');
+        // validateTags($('#tags').val().trim(), 'Home');
+    } catch (e) {
+        event.preventDefault();
+        $('#errorDiv').append(`<p>Error: ${e}</p>`);
+        $('#errorDiv').show();
     }
-  });
 });
