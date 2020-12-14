72e5fd2f52c0c94b4e1fa0ee476befc8cd4e881f
Jordan Handwerger <jordanhandwerger@yahoo.com>
2020-12-13 22:32:50 -0500

    Add front-end edit task functionality - same as the profile edit view - edit, update, cancel buttons - locking input functionilty - resetting input

---
+++ public/js/taskDetails.js
@@ -0,0 +1,123 @@
+let title, description, priority, dueDate, reminderDate, status, assignee, tags;
+
+function disableInput() {
+    $('input').attr('disabled', true);
+    $('select').attr('disabled', true);
+    $('textarea').attr('disabled', true);
+}
+
+function enableInput() {
+    $('input').attr('disabled', false);
+    $('select').attr('disabled', false);
+    $('textarea').attr('disabled', false);
+}
+
+function disableForm() {
+    disableInput();
+    $('#updateTaskButton').hide();
+    $('#editTaskButton').show();
+    $('#cancelEditButton').hide();
+}
+
+function enableForm() {
+    getOriginalTaskInfo();
+    enableInput();
+    $('#updateTaskButton').show();
+    $('#editTaskButton').hide();
+    $('#cancelEditButton').show();
+}
+
+function getOriginalTaskInfo() {
+    title = $('#title').val().trim();
+    description = $('#description').val().trim();
+    priority = $('#priority').val().trim();
+    dueDate = $('#dueDate').val().trim();
+    reminderDate = $('#reminderDate').val().trim();
+    status = $('#status').val().trim();
+    assignee = $('#assignee').val().trim();
+    tags = $('#tags').val().trim();
+}
+
+function cancelTaskUpdate() {
+    $('#title').val(title);
+    $('#description').val(description);
+    $('#priority').val(priority);
+    $('#dueDate').val(dueDate);
+    $('#reminderDate').val(reminderDate);
+    $('#status').val(status);
+    $('#assignee').val(assignee);
+    $('#tags').val(tags);
+    clearErrors();
+    disableForm();
+}
+
+function validateTaskUpdates() {
+    $('#errorDiv').hide();
+    $('#errorDiv').empty();
+    validateStringInput($('#title').val().trim(), 'Title');
+    validateStringInput($('#description').val().trim(), 'Description');
+    validateSelect($('#priority').val().trim(), 'Priority');
+    validateDate($('#dueDate').val().trim(), 'Due Date');
+    validateDate($('#reminderDate').val().trim(), 'Reminder Date');
+    validateSelect($('#status').val().trim(), 'Status');
+    validateStringInput($('#assignee').val().trim(), 'Assignee');
+    validateTags($('#tags').val().trim());
+}
+
+function getFormValues() {
+    return {
+        title: $('#title').val().trim(),
+        description: $('#description').val().trim(),
+        priority: $('#priority').val().trim(),
+        dueDate: $('#dueDate').val().trim(),
+        reminderDate: $('#reminderDate').val().trim(),
+        status: $('#status').val().trim(),
+        assignee: $('#assignee').val().trim(),
+        tags: $('#tags').val().trim(),
+    };
+}
+
+function handleAJAXError(error) {
+    $('#errorDiv').append(
+        `<p>There was an error while updating your Task. Please try again later.</p>`
+    );
+    $('#errorDiv').show();
+}
+
+function clearErrors() {
+    $('#errorDiv').empty();
+    $('#errorDiv').hide();
+}
+
+function updateTaskWithAJAX() {
+    const id = $('#id').val().trim();
+    const requestConfig = {
+        method: 'POST',
+        url: `/tasks/${id}`,
+        data: JSON.stringify(getFormValues()),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: disableForm,
+        error: handleAJAXError,
+    };
+
+    $.ajax(requestConfig);
+}
+
+$(disableForm);
+
+$('#editTaskButton').on('click', enableForm);
+
+$('#updateTaskButton').on('click', (event) => {
+    event.preventDefault();
+    try {
+        validateTaskUpdates();
+        disableForm();
+        updateTaskWithAJAX();
+    } catch (e) {
+        $('#errorDiv').append(`<p>Error: ${e}</p>`);
+        $('#errorDiv').show();
+    }
+});
+
+$('#cancelEditButton').on('click', cancelTaskUpdate);
