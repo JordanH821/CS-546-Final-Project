fcc43483847f44e84148b813fc297fd53b880caf
Jordan Handwerger <jordanhandwerger@yahoo.com>
2020-12-17 01:24:26 -0500

    Update dashboard routes error handling

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -15,64 +15,73 @@ router.get(
     '/',
     authenticationCheckRedirect('/users/login', true),
     async (req, res) => {
-        let tasks;
-        let searchTerm;
-        let tagSet = new Set();
-        let tags;
-        let tag;
-        if (req.query && req.query.searchTerm) {
-            searchTerm = xss(req.query.searchTerm);
-            try {
-                searchTerm = validateStringInput(searchTerm);
-                tasks = await usersData.searchUsersTasks(
+        try {
+            let tasks;
+            let searchTerm;
+            let tagSet = new Set();
+            let tags;
+            let tag;
+            if (req.query && req.query.searchTerm) {
+                searchTerm = xss(req.query.searchTerm);
+                try {
+                    searchTerm = validateStringInput(searchTerm);
+                    tasks = await usersData.searchUsersTasks(
+                        req.session.user._id,
+                        searchTerm
+                    );
+                } catch (e) {
+                    console.log(`Error searching tasks: ${e}`);
+                    tasks = await usersData.getAllTasksForUser(
+                        req.session.user._id
+                    );
+                }
+            } else if (req.query && req.query.tag) {
+                tag = validateStringInput(req.query.tag);
+                tasks = await usersData.getUsersTasksByTag(
                     req.session.user._id,
-                    searchTerm
+                    tag
                 );
-            } catch (e) {
-                console.log(`Error searching tasks: ${e}`);
+            } else {
                 tasks = await usersData.getAllTasksForUser(
                     req.session.user._id
                 );
             }
-        } else if (req.query && req.query.tag) {
-            tag = validateStringInput(req.query.tag);
-            tasks = await usersData.getUsersTasksByTag(
-                req.session.user._id,
-                tag
+
+            tasks
+                .filter((task) => task.status != 'Archived')
+                .map((task) => task.tags.map((t) => tagSet.add(t)));
+            tags = Array.from(tagSet);
+
+            let taskNotifications = await getTaskNotificationsForUser(
+                req.session.user._id
             );
-        } else {
-            tasks = await usersData.getAllTasksForUser(req.session.user._id);
+            res.render('dashboard/dashboard', {
+                title: 'Dashboard',
+                user: req.session.user,
+                toDoCards: tasksData.sortTasksByDate(
+                    tasks.filter((task) => task.status == 'To Do'),
+                    false
+                ),
+                inProgressCards: tasksData.sortTasksByDate(
+                    tasks.filter((task) => task.status == 'In Progress'),
+                    false
+                ),
+                doneCards: tasksData.sortTasksByDate(
+                    tasks.filter((task) => task.status == 'Done'),
+                    false
+                ),
+                searchTerm: searchTerm,
+                tag: tag,
+                tags: tags,
+                taskNotifications: taskNotifications,
+            });
+        } catch (e) {
+            console.log(e.toString());
+            res.status(500).render('error/500', {
+                title: 'Server Error',
+                error: 'An error occur while preparing the dashboard page.',
+            });
         }
-
-        tasks
-            .filter((task) => task.status != 'Archived')
-            .map((task) => task.tags.map((t) => tagSet.add(t)));
-        tags = Array.from(tagSet);
-
-        let taskNotifications = await getTaskNotificationsForUser(
-            req.session.user._id
-        );
-
-        res.render('dashboard/dashboard', {
-            title: 'Dashboard',
-            user: req.session.user,
-            toDoCards: tasksData.sortTasksByDate(
-                tasks.filter((task) => task.status == 'To Do'),
-                false
-            ),
-            inProgressCards: tasksData.sortTasksByDate(
-                tasks.filter((task) => task.status == 'In Progress'),
-                false
-            ),
-            doneCards: tasksData.sortTasksByDate(
-                tasks.filter((task) => task.status == 'Done'),
-                false
-            ),
-            searchTerm: searchTerm,
-            tag: tag,
-            tags: tags,
-            taskNotifications: taskNotifications,
-        });
     }
 );
 
